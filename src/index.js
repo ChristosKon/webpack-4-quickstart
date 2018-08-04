5ced5efb80268ac6d36957d5e2bb9b6b58caa8ac
christoskon <xristoskonsta@gmail.com>
2018-08-04 23:35:25 +0300

    [Test] Add Copy Webpack Plugin, Add map.html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+//import { BrowserRouter } from 'react-router-dom'
+import App from "./components/App";
+import Nav from "./components/Nav";
 import style from "./main.css";
 
+ReactDOM.render(<Nav />, document.getElementById("nav"));
 ReactDOM.render(<App />, document.getElementById("app"));
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 
 module.exports = {
   module: {
@@ -31,9 +33,17 @@ module.exports = {
       template: "./src/index.html",
       filename: "./index.html"
     }),
+    new HtmlWebPackPlugin({
+      template: "./src/map.html",
+      filename: "./map.html"
+    }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css"
-    })
+    }),
+    new CopyWebpackPlugin([{      
+      from: "./_redirects",
+      to: './',
+    }], { debug: 'info' })
   ]
 };
